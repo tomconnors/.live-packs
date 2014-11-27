40e49ec675ef6d96ad3ec8c0f223a946c58ad63c
Tom Connors <tvconnors@gmail.com>
2014-11-27 00:00:54 -0500

    got emacs working inside tmux

--- tom-pack/init.el
+++ tom-pack/init.el
@@ -6,7 +6,7 @@
 ;; Load bindings config
 (live-load-config-file "bindings.el")
 
-;; maintain old key bindings
+;; maintain old key bindings, color schemes
 (live-ignore-packs '(stable/bindings-pack stable/colour-pack))
 
 ;; to specify exactly what packs I want:
@@ -23,7 +23,8 @@
 ;; for line numbers
 (require 'linum)
 (global-linum-mode)
-
+;; add a handsome space between the line numbers and the actual text in the buffer
+(setq linum-format "%d ")
 
 ;; get rid of fancy f's replacing regular old fn's
 (add-hook 'clojure-mode-hook
@@ -46,9 +47,6 @@
 (when (fboundp 'windmove-default-keybindings)
   (windmove-default-keybindings))
 
-;; split window at start up
-;; (add-hook 'emacs-startup-hook (split-window-horizontally))
-
 ;; turn off yas tab-expansions
 ;; (i'd like to be able to expand with C-TAB, but can't figure it out.)
 (define-key yas-minor-mode-map (kbd "TAB") nil)
@@ -63,9 +61,6 @@
 ;; smaller font
 (set-face-attribute 'default nil :height 102)
 
-;; hopefully this speeds everything up?
-;;(byte-recompile-directory (expand-file-name "~/.emacs.d") 0)
-
 ;; this makes the right command key on a mac function as a control key
 (setq ns-right-command-modifier 'control)
 
@@ -75,7 +70,6 @@
 (live-load-config-file "coffee-conf.el")
 (live-load-config-file "less-conf.el")
 
-
 ;; fn for killing all dired mode buffers
 (defun kill-dired-buffers ()
 	 (interactive)
@@ -84,8 +78,103 @@
              (kill-buffer buffer)))
          (buffer-list)))
 
-;; removes emac live's cute scratch buffer nonsense
-(setq initial-scratch-message "")
-
 ;; 2 space indents for js
 (setq js-indent-level 2)
+
+;; handle tmux's xterm-keys
+;; put the following line in your ~/.tmux.conf:
+;;   setw -g xterm-keys on
+(if (getenv "TMUX")
+    (progn
+      (let ((x 2) (tkey ""))
+    (while (<= x 8)
+      ;; shift
+      (if (= x 2)
+          (setq tkey "S-"))
+      ;; alt
+      (if (= x 3)
+          (setq tkey "M-"))
+      ;; alt + shift
+      (if (= x 4)
+          (setq tkey "M-S-"))
+      ;; ctrl
+      (if (= x 5)
+          (setq tkey "C-"))
+      ;; ctrl + shift
+      (if (= x 6)
+          (setq tkey "C-S-"))
+      ;; ctrl + alt
+      (if (= x 7)
+          (setq tkey "C-M-"))
+      ;; ctrl + alt + shift
+      (if (= x 8)
+          (setq tkey "C-M-S-"))
+
+      ;; arrows
+      (define-key key-translation-map (kbd (format "M-[ 1 ; %d A" x)) (kbd (format "%s<up>" tkey)))
+      (define-key key-translation-map (kbd (format "M-[ 1 ; %d B" x)) (kbd (format "%s<down>" tkey)))
+      (define-key key-translation-map (kbd (format "M-[ 1 ; %d C" x)) (kbd (format "%s<right>" tkey)))
+      (define-key key-translation-map (kbd (format "M-[ 1 ; %d D" x)) (kbd (format "%s<left>" tkey)))
+      ;; home
+      (define-key key-translation-map (kbd (format "M-[ 1 ; %d H" x)) (kbd (format "%s<home>" tkey)))
+      ;; end
+      (define-key key-translation-map (kbd (format "M-[ 1 ; %d F" x)) (kbd (format "%s<end>" tkey)))
+      ;; page up
+      (define-key key-translation-map (kbd (format "M-[ 5 ; %d ~" x)) (kbd (format "%s<prior>" tkey)))
+      ;; page down
+      (define-key key-translation-map (kbd (format "M-[ 6 ; %d ~" x)) (kbd (format "%s<next>" tkey)))
+      ;; insert
+      (define-key key-translation-map (kbd (format "M-[ 2 ; %d ~" x)) (kbd (format "%s<delete>" tkey)))
+      ;; delete
+      (define-key key-translation-map (kbd (format "M-[ 3 ; %d ~" x)) (kbd (format "%s<delete>" tkey)))
+      ;; f1
+      (define-key key-translation-map (kbd (format "M-[ 1 ; %d P" x)) (kbd (format "%s<f1>" tkey)))
+      ;; f2
+      (define-key key-translation-map (kbd (format "M-[ 1 ; %d Q" x)) (kbd (format "%s<f2>" tkey)))
+      ;; f3
+      (define-key key-translation-map (kbd (format "M-[ 1 ; %d R" x)) (kbd (format "%s<f3>" tkey)))
+      ;; f4
+      (define-key key-translation-map (kbd (format "M-[ 1 ; %d S" x)) (kbd (format "%s<f4>" tkey)))
+      ;; f5
+      (define-key key-translation-map (kbd (format "M-[ 15 ; %d ~" x)) (kbd (format "%s<f5>" tkey)))
+      ;; f6
+      (define-key key-translation-map (kbd (format "M-[ 17 ; %d ~" x)) (kbd (format "%s<f6>" tkey)))
+      ;; f7
+      (define-key key-translation-map (kbd (format "M-[ 18 ; %d ~" x)) (kbd (format "%s<f7>" tkey)))
+      ;; f8
+      (define-key key-translation-map (kbd (format "M-[ 19 ; %d ~" x)) (kbd (format "%s<f8>" tkey)))
+      ;; f9
+      (define-key key-translation-map (kbd (format "M-[ 20 ; %d ~" x)) (kbd (format "%s<f9>" tkey)))
+      ;; f10
+      (define-key key-translation-map (kbd (format "M-[ 21 ; %d ~" x)) (kbd (format "%s<f10>" tkey)))
+      ;; f11
+      (define-key key-translation-map (kbd (format "M-[ 23 ; %d ~" x)) (kbd (format "%s<f11>" tkey)))
+      ;; f12
+      (define-key key-translation-map (kbd (format "M-[ 24 ; %d ~" x)) (kbd (format "%s<f12>" tkey)))
+      ;; f13
+      (define-key key-translation-map (kbd (format "M-[ 25 ; %d ~" x)) (kbd (format "%s<f13>" tkey)))
+      ;; f14
+      (define-key key-translation-map (kbd (format "M-[ 26 ; %d ~" x)) (kbd (format "%s<f14>" tkey)))
+      ;; f15
+      (define-key key-translation-map (kbd (format "M-[ 28 ; %d ~" x)) (kbd (format "%s<f15>" tkey)))
+      ;; f16
+      (define-key key-translation-map (kbd (format "M-[ 29 ; %d ~" x)) (kbd (format "%s<f16>" tkey)))
+      ;; f17
+      (define-key key-translation-map (kbd (format "M-[ 31 ; %d ~" x)) (kbd (format "%s<f17>" tkey)))
+      ;; f18
+      (define-key key-translation-map (kbd (format "M-[ 32 ; %d ~" x)) (kbd (format "%s<f18>" tkey)))
+      ;; f19
+      (define-key key-translation-map (kbd (format "M-[ 33 ; %d ~" x)) (kbd (format "%s<f19>" tkey)))
+      ;; f20
+      (define-key key-translation-map (kbd (format "M-[ 34 ; %d ~" x)) (kbd (format "%s<f20>" tkey)))
+
+      (setq x (+ x 1))
+      ))
+    )
+  )
+
+;; fix yanking under tmux
+(if (getenv "TMUX")
+    (progn
+      (setq interprogram-cut-function nil)
+      (setq interprogram-paste-function nil)))
